bd924b8ddb868a55d2c7695de9651b782f68cb67
Steve Barman <steve@stevebarman.com>
2017-11-09 21:58:08 -0500

    adds condition for last page

--- src/content.js
+++ src/content.js
@@ -2,7 +2,9 @@ chrome.runtime.onMessage.addListener(
   function(request) {
     if( request.message === "clicked_browser_action" ) {
       var nextButton = document.querySelector(".next-button > a");
-      location.replace(nextButton.getAttribute("href"));
+      if (nextButton) {
+        location.replace(nextButton.getAttribute("href"));        
+      }
     }
   }
 );
