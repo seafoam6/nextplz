4eb9a79e5245c90adacad8681afc87f9679fb6f1
Steve Barman <steve@stevebarman.com>
2017-11-09 21:13:43 -0500

    removes jQuery

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener(
   function(request) {
     if( request.message === "clicked_browser_action" ) {
-      var nextButton = $(".next-button > a").attr("href");
-      location.replace(nextButton);
+      var nextButton = document.querySelector(".next-button > a");
+      location.replace(nextButton.getAttribute("href"));
     }
   }
 );
