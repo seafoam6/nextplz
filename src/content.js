9848b610b686a9d42501e18de7b02d27e1943348
Steve Barman <steve@stevebarman.com>
2017-11-10 16:41:39 -0500

    uses event listener, goes left/right

--- src/content.js
+++ src/content.js
@@ -1,10 +1,22 @@
-chrome.runtime.onMessage.addListener(
-  function(request) {
-    if( request.message === "clicked_browser_action" ) {
-      var nextButton = document.querySelector(".next-button > a");
-      if (nextButton) {
-        location.replace(nextButton.getAttribute("href"));        
-      }
-    }
+function moveLeft(){
+  var nextButton = document.querySelector(".prev-button > a");
+  if (nextButton) {
+    location.replace(nextButton.getAttribute("href"));        
   }
-);
+}
+
+function moveRight(){
+  var nextButton = document.querySelector(".next-button > a");
+  if (nextButton) {
+    location.replace(nextButton.getAttribute("href"));        
+  }
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key == 'ArrowRight'){
+    moveRight();
+  }
+  if (event.key == 'ArrowLeft'){
+    moveLeft();
+  }
+});
